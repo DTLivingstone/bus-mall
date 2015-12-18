c57ef82bcf5d1b10af9eb595182d5dfeaadbfc33
Paul Sheridan <paul.sheridan@me.com>
2015-12-18 09:31:00 -0800

    linted code, removed errors

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // product constructor and prototypes
 var allProducts = [];
-var productNames = ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine_glass"];
+var productNames = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb', 'water_can', 'wine_glass'];
 
 var Product = function(desc, image, views, clicks) {
   this.desc = desc;
@@ -10,9 +10,9 @@ var Product = function(desc, image, views, clicks) {
 };
 
 // populate allProducts
-for (i = 0; i < productNames.length; i += 1) {
-    console.log(productNames[i]);
-    var newProduct;
-    newProduct = new Product(productNames[i], "img/" + productNames[i] + ".jpg", 0, 0);
-    allProducts.push([productNames[i], newProduct]);
+for (var i = 0; i < productNames.length; i += 1) {
+  console.log(productNames[i]);
+  var newProduct;
+  newProduct = new Product(productNames[i], 'img/' + productNames[i] + '.jpg', 0, 0);
+  allProducts.push([productNames[i], newProduct]);
 }
